7f33d74853f1799ae2326827b6954429b1c9ba7b
vinjn <vinjn.z@gmail.com>
2016-08-08 22:22:46 +0800

    Hack depthMap.fs

--- assets/depthMap.fs
+++ assets/depthMap.fs
@@ -6,6 +6,6 @@ void main(void)
 { 
     // sample from the texture  
     float r = texture2D(image, gl_TexCoord[0].st).r;
-    r = r * 65536 / 8000;
+    r = r * 65536 / 4000;
     gl_FragColor = vec4(1, 1, 1, 1);
 }
